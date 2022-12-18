f08b31d114777954b91f0055f5fa840896cbdc03
Ashley <ashley.tharp@gmail.com>
2022-12-17 18:15:22 -0600

    comments created for customer and appointment report controllers

--- src/Controller/AppointmentReportController.java
+++ src/Controller/AppointmentReportController.java
@@ -11,54 +11,110 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+/**
+ * This is the controller class for the appt-reports.fxml view. You can see this controller set in the root node of that
+ * fxml file with this node property:
+ * ```
+ *      fx:controller="Controller.AppointmentReportController"
+ * ```
+ *
+ *  This view has a few tabs, where the user can select what they want to see and see a text string report of the results
+ *  fetched from the database. For example, users can view a report generated for appointments by Type, by Month, and/or
+ *  by Contact.
+ *
+ */
 public class AppointmentReportController implements Initializable {
 
+    /**
+     * Allows the user to select appointment Type and see all appointments with matching type shown in a text format
+     * below the ComboBox
+     */
     @FXML
     private ComboBox apptTypeComboBox;
 
+    /**
+     * Allows the user to select appointment month and see all appointments with matching month shown in a text format
+     * below the ComboBox
+     */
     @FXML
     private ComboBox apptMonthComboBox;
 
+    /**
+     * Allows the user to select appointment Contact and see all appointments with matching Contact shown in a text format
+     * below the ComboBox
+     */
     @FXML
     private ComboBox contactComboBox;
 
+    /**
+     * Shows the number of appointments returned matching chosen Type in integer form.
+     */
     @FXML
     private Label numApptsLabelType;
 
+    /**
+     * Shows the number of appointments returned matching chosen month in integer form.
+     */
     @FXML
     private Label numApptsLabelMonth;
 
+    /**
+     * Shows the number of appointments returned matching chosen Contact in integer form.
+     */
     @FXML
     private Label numApptsLabelContact;
 
+    /**
+     * The text area showing the report for appointments matching the chosen Type
+     */
     @FXML
     private TextArea reportTextAreaType;
 
+    /**
+     * The text area showing the report for appointments matching the chosen month
+     */
     @FXML
     private TextArea reportTextAreaMonth;
 
+    /**
+     * The text area showing the report for appointments matching the chosen Contact
+     */
     @FXML
     private TextArea reportTextAreaContact;
 
+    /**
+     * Called when the appt-reports.fxml file is loaded. This function initializes some view items with list of
+     * Strings created from items returned from the database.
+     * @param url - not used
+     * @param resourceBundle - not used
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Init Type ComboBox
         try {
             ObservableList<String> types = FXCollections.observableList(DBAppointment.getUniqueAppointmentTypes());
             apptTypeComboBox.setItems(types);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        // init month and contact combobox
         UXUtil.initMonthComboBox(apptMonthComboBox);
         UXUtil.initContactComboBox(contactComboBox);
-
     }
-    public void onTypeSelected(ActionEvent actionEvent) throws IOException, SQLException {
+
+    /**
+     * Called after user selects a Type from the ComboBox. The appropriate TextArea will be populated with a text
+     * report of all the appointments returned from the database that match the chosen type.
+     *
+     * @param actionEvent - not used
+     * @throws SQLException - thrown if database cannot find a match
+     */
+    public void onTypeSelected(ActionEvent actionEvent) throws SQLException {
         String type = apptTypeComboBox.getValue().toString();
         ArrayList<Appointment> appointments = DBAppointment.getAllAppointmentsOfType(type);
         numApptsLabelType.setText(Integer.toString(appointments.size()));
@@ -70,7 +126,14 @@ public class AppointmentReportController implements Initializable {
         reportTextAreaType.setText(s);
     }
 
-    public void onMonthSelected(ActionEvent actionEvent) throws IOException, SQLException {
+    /**
+     * Called after user selects a month from the ComboBox. The appropriate TextArea will be populated with a text
+     * report of all the appointments returned from the database that match the chosen month.
+     *
+     * @param actionEvent - not used
+     * @throws SQLException - thrown if database cannot find a match
+     */
+    public void onMonthSelected(ActionEvent actionEvent) throws SQLException {
         String month = apptMonthComboBox.getValue().toString();
         ArrayList<Appointment> appointments = DBAppointment.getAllAppointmentsInMonth(month);
         numApptsLabelMonth.setText(Integer.toString(appointments.size()));
@@ -82,7 +145,14 @@ public class AppointmentReportController implements Initializable {
         reportTextAreaMonth.setText(s);
     }
 
-    public void onContactSelected(ActionEvent actionEvent) throws Exception {
+    /**
+     * Called after user selects a Contact from the ComboBox. The appropriate TextArea will be populated with a text
+     * report of all the appointments returned from the database that match the chosen Contact.
+     *
+     * @param actionEvent - not used
+     * @throws SQLException - thrown if database cannot find a match
+     */
+    public void onContactSelected(ActionEvent actionEvent) throws SQLException {
         int contactId = UXUtil.getIdNumberFromComboBox(contactComboBox);
         ArrayList<Appointment> appointments = DBAppointment.getAllAppointmentsForContact(contactId);
         numApptsLabelContact.setText(Integer.toString(appointments.size()));
--- src/Controller/CustomerReportController.java
+++ src/Controller/CustomerReportController.java
@@ -17,23 +17,56 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+/**
+ * This is the controller class for the customer-report.fxml view. You can see this controller set in the root node of that
+ * fxml file with this node property:
+ * ```
+ *      fx:controller="Controller.CustomerReportController"
+ * ```
+ * This view shows a ComboBox the user can use to select a Customer they want to see a report generated for.
+ * The list of all customers is fetched from the database and used to populate this combobox.
+ * The report shows in a text format the set of all appointments for that customer.
+ *
+ */
 public class CustomerReportController  implements Initializable {
 
+    /**
+     * Allows the user to select the Customer they want to see the report generated for. The report will contain the list
+     * of all appointments for the selected customer.
+     */
     @FXML
     private ComboBox customerComboBox;
 
+    /**
+     * Shows the number of appointments returned for the chosen Customer.
+     */
     @FXML
     private Label customerApptNumLabel;
 
+    /**
+     * Displays the generated text report showing a list of all the appointments for the chosen customer in no
+     * particular order.
+     */
     @FXML
     private TextArea reportTextAreaCustomer;
 
+    /**
+     * Called when customer-report.fxml is loaded. This function initilizes the Customer ComboBox with the set of all
+     * Customers returned from the Database.
+     * @param url - not used
+     * @param resourceBundle - not used
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         UXUtil.initCustomerIDComboBox(customerComboBox);
     }
 
-    public void onCustomerSelected(ActionEvent actionEvent) throws IOException, SQLException {
+    /**
+     * Called when the user selects a Customer from the ComboBox. On customer selected, a text report will be generated
+     * and displayed in the TextArea showing the set of all appointments for that Customer in no particular order.
+     * @param actionEvent - not used
+     */
+    public void onCustomerSelected(ActionEvent actionEvent) {
         int customerId = UXUtil.getIdNumberFromComboBox(customerComboBox);
         ObservableList<Appointment> appointments = DBAppointment.lookupAppointmentsForCustomer(customerId);
         customerApptNumLabel.setText(Integer.toString(appointments.size()));
